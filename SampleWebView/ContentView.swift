7000f5a659df9d245dd3c3c0b30e06d08f270e31
Charly MI <charly_1310@outlook.com>
2024-07-11 23:26:24 -0600

    Initial Commit

---
+++ SampleWebView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleWebView
+//
+//  Created by wastecross on 7/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleWebView/SampleWebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleWebViewApp.swift
+//  SampleWebView
+//
+//  Created by wastecross on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleWebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
